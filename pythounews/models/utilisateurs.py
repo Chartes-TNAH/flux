eea1c81d79f099da588fadd0bda613832990bcdd
HeathcoteA <amandine.heathcote@enc-sorbonne.fr>
2018-03-22 10:56:25 +0100

    reprise de modification du message d'erreur promo issue #71

--- pythounews/models/utilisateurs.py
+++ pythounews/models/utilisateurs.py
@@ -41,7 +41,8 @@ class User(UserMixin, db.Model):
         :param email: Email de l'utilisateur-rice
         :param nom: Nom de l'utilisateur-rice
         :param motdepasse: Mot de passe de l'utilisateur-rice (Minimum 6 caractères)
-        :param promo: année de promo de l'utilisateur-rice (forcément integer)
+        :param promo: Année de promotion de l'utilisateur-rice
+
 
         """
         erreurs = []
@@ -53,7 +54,7 @@ class User(UserMixin, db.Model):
             erreurs.append("Le nom fourni est vide")
         if not motdepasse or len(motdepasse) < 6:
             erreurs.append("Le mot de passe fourni est vide ou trop court")
-        if not promo or str(promo):
+        if not promo or not type(promo)==str :
             erreurs.append("La promo fournie est vide ou ne correspond pas à une année")
 
         # On vérifie que personne n'a utilisé cet email ou ce login
